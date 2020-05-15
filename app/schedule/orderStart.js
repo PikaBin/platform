cfe6bd644a3af04ff6ee4a577fdc89f427a2c62c
PiKaBin <1747320015@qq.com>
2020-05-15 11:46:30 +0800

    新增定时器

---
+++ app/schedule/orderStart.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const Subscription = require('egg').Subscription;
+
+
+class OrderStart extends Subscription {
+  static get schedule() {
+    return {
+      interval: '10s',
+      // cron: '*/15 * * * * *',
+      type: 'all',
+    };
+  }
+
+  async subscribe() {
+    // await this.ctx.service.workorder.workorderAdd();
+    // console.log(1);
+  }
+}
+
+module.exports = OrderStart;
